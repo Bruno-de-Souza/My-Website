8e9f838a58f28820bda956bcbed5368da1f81272
Bruno-de-Souza <brunomoreiramg@hotmail.com>
2022-08-20 23:46:37 -0300

    Committing modified files.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,39 @@
 //! === ADD HEADER STYLES ON SCROLL ===
 const headerScroll = () => {
-    const headerElement = document.querySelector('.header');
-    this.scrollY >= 30 ? headerElement.classList.add('active') : headerElement.classList.remove('active');
-}
-window.addEventListener('scroll', headerScroll);  
+  const headerElement = document.querySelector(".header");
+  this.scrollY >= 30
+    ? headerElement.classList.add("active")
+    : headerElement.classList.remove("active");
+};
+window.addEventListener("scroll", headerScroll);
 //! === OPEN AND CLOSE THE MENU ON HAMBURGER ICON CLICK ===
-const menuToggler = document.querySelector('#menu-toggler');
-const navbarMenu = document.querySelector('.navbar_menu');
+const menuToggler = document.querySelector("#menu-toggler");
+const navbarMenu = document.querySelector(".navbar_menu");
 const toggleMenu = () => {
-    navbarMenu.classList.toggle('active');
-}
-menuToggler.addEventListener('click', toggleMenu);
+  navbarMenu.classList.toggle("active");
+};
+menuToggler.addEventListener("click", toggleMenu);
 // --- CLOSE MENU WHEN NAV-LINKS ARE CLICKED ---
 const linksToggleMenu = (e) => {
-    if (e.target.classList.contains('navbar_list-link')) navbarMenu.classList.remove('active');
-}
-window.addEventListener('click', linksToggleMenu);
+  if (e.target.classList.contains("navbar_list-link"))
+    navbarMenu.classList.remove("active");
+};
+window.addEventListener("click", linksToggleMenu);
 //! === SWIPER ===
-
+const swiper = new Swiper(".myswiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 50,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
 //! === SCROLL REVEAL ===
